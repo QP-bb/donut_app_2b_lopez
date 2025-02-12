816f73f19facece1664abfcc4f169ba2a1d39e17
Eri Lopez <erilopezmendoza@outlook.com>
2025-02-12 11:27:51 -0600

    TabBar Added

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:donut_app_2b_lopez/utils/my_tab.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -8,9 +9,24 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  //Lista de tabs
+  List<Widget> myTabs = [
+    //DonutTab
+    MyTab(iconPath: 'lib/icons/donut.png',),
+    //BurgerTab
+    MyTab(iconPath: 'lib/icons/burger.png',),
+    //SmoothieTab
+    MyTab(iconPath: 'lib/icons/smoothie.png',),
+    //PancakeTab
+    MyTab(iconPath: 'lib/icons/pancakes.png',),
+    //PizzaTab
+    MyTab(iconPath: 'lib/icons/pizza.png',),
+  ];
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return DefaultTabController(
+      length:5,
+      child: Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.transparent,
         //Icono iquzquierdo
@@ -19,9 +35,37 @@ class _HomePageState extends State<HomePage> {
           color: Colors.grey[800],
           ),
           //Icono derecho
-          actions: [
-            Icon(Icons.person)],
-        ),
-    );
+          actions: const [
+            Padding(
+            padding: EdgeInsets.only(right:24.0),
+            child: Icon(Icons.person),
+            ),
+          ],
+      ),
+        body: Column(
+          children: [
+            //Texto principal
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 36, vertical: 18),
+              child: Row(
+                children: [
+                  Text("I want to ", style: TextStyle(fontSize:32)),
+                  Text("Eat", style: TextStyle(
+                    //Tamaño de letra
+                    fontSize:32,
+                    //Negritas
+                    fontWeight: FontWeight.bold,
+                    //Subrayado
+                    decoration: TextDecoration.underline)),
+                ],
+              ),
+            ),
+            //TabBar
+            TabBar(tabs: myTabs)
+            //TabBarView
+
+            //Carrito
+          ],)
+    ));
   }
 }
