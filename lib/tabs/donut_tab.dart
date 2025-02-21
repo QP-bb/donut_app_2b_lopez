f874664483e6265d79df117b886ea7d784201660
Eri Lopez <erilopezmendoza@outlook.com>
2025-02-21 11:37:34 -0600

    Starting GridView

--- lib/tabs/donut_tab.dart
+++ lib/tabs/donut_tab.dart
@@ -1,24 +1,28 @@
 import 'package:flutter/material.dart';
+import '../utils/donut_tile.dart';
 
 class DonutTab extends StatelessWidget {
 
   //Lista de donas
   final List donutsOnSale =[
-    //[donutFlavor, donutPrice, donutColor, imageName]
+    //[donutFlavor, donutPrice, donutColor, imageName],
     ["Ice Cream", "36", Colors.blue, "lib/images/icecream_donut.png"],
     ["Strawberry", "45", Colors.red, "lib/images/strawberry_donut.png"],
     ["Grape Ape", "84", Colors.purple, "lib/images/grape_donut.png"],
-    ["Choco", "95", Colors.brown, "lib/images/chocolate_donut.png"],
-  ];
+    ["Choco", "95", Colors.brown, "lib/images/chocolate_donut.png"]];
 
   DonutTab({super.key});
 
   @override
   Widget build(BuildContext context) {
+    //Para acomodar elementos en Cuadricula
     return GridView.builder(
+      //Cuantos elementos tengo
       itemCount: donutsOnSale.length,
-      padding: const EdgeInsets.all(6.0),
+      padding: const EdgeInsets.all(8.0),
+      //Organiza como distribuir elementos en la cuadricula
       gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        //Numero de columnas
         crossAxisCount: 2),
       itemBuilder: (context, index) {
         return DonutTile(
