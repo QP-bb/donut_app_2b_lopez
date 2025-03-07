4d10934f8b1fe6fb317822ed2f85be3426cec420
Eri Lopez <erilopezmendoza@outlook.com>
2025-03-07 11:46:47 -0600

    DonutTile Finished

--- lib/utils/donut_tile.dart
+++ lib/utils/donut_tile.dart
@@ -2,12 +2,20 @@ import 'package:flutter/material.dart';
 
 class DonutTile extends StatelessWidget {
   final String donutFlavor;
+  final String donutStore;
   final String donutPrice;
   //dynamic = puede ser cualquier tipo de dato
   final dynamic donutColor;
   final String imageName;
 
-  const DonutTile({super.key, required this.donutFlavor, required this.donutPrice, this.donutColor, required this.imageName});
+  const DonutTile(
+    {super.key, 
+    required this.donutFlavor, 
+    required this.donutStore,
+    required this.donutPrice, 
+    this.donutColor, 
+    required this.imageName}
+    );
 
   @override
   Widget build(BuildContext context) {
@@ -44,12 +52,29 @@ class DonutTile extends StatelessWidget {
             Padding(
               padding: const EdgeInsets.symmetric(vertical:12, horizontal: 34),
               child: Image.asset(imageName),
-            )
+            ),
             //Donut Text
-
+            Text(donutFlavor,
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20,)),
+            //Texto con espacio
+            const SizedBox(height: 4),
+            //Agregar todo el texto
+            Text(
+              donutStore,
+              style: TextStyle(color: Colors.grey[600]),
+            ),
+              Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Icon(Icons.favorite_border, color: Colors.pink[400]),
+                ],
+              ),
+            ),
             //Love icon + add button
           ],
-        )
+        ),
       ),
     );
   }
